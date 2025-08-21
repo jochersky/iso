90617518158f1a4d176963c91e9238e880321579
Josh Chernetsky <joshcherndev@gmail.com>
2025-08-21 14:27:23 -0700

    Level edit mode groundwork finished
    * separate camera movement on toggle working
    
    * fixed issue with camera wasd movement
    
    * Camera farther away and far and near adjusted
    
    * Actually fixed camera rotation and player move orientation
    
    * Blocks translatable on single plane
    
    * Fixed collision in blocks where there are holes

---
+++ Assets/Scripts/Player/BlockSelect.cs
@@ -0,0 +1,88 @@
+using System;
+using UnityEngine;
+
+public class BlockSelect : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    public LayerMask mask;
+    [SerializeField] float rayDistance = 1000f;
+
+    [SerializeField] StateSelect stateSelect;
+    bool playEnabled = true;
+    bool blockSelected;
+    bool _movingBlock;
+    RaycastHit selectedBlock;
+
+    private void OnEnable()
+    {
+        stateSelect.togglePressed += ChangePlayState;
+    }
+
+    private void OnDisable()
+    {
+        stateSelect.togglePressed -= ChangePlayState;
+    }
+
+    void Update()
+    {
+        DetectBlock();
+    }
+
+    private void DetectBlock()
+    {
+        if (!playEnabled)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                _movingBlock = false;
+                blockSelected = false;
+            }
+
+            if (Input.GetMouseButton(0))
+                {
+                    _movingBlock = true;
+
+                    // Check if the player is trying to select a block to translate.
+                    if (!blockSelected)
+                    {
+                        Ray mouseRay = cam.ScreenPointToRay(Input.mousePosition);
+                        RaycastHit hit;
+
+                        if (Physics.Raycast(mouseRay, out hit, rayDistance, mask))
+                        {
+                            blockSelected = true;
+                            selectedBlock = hit;
+                        }
+                    }
+                }
+
+            // TODO: make it so that the player needs to drag on the move icon to translate a block.
+            if (_movingBlock)
+            {
+                Plane translationPlane = new Plane(Vector3.down, 0);
+                Ray mouseRay = cam.ScreenPointToRay(Input.mousePosition);
+
+                if (translationPlane.Raycast(mouseRay, out float distance))
+                {
+                    Vector3 newPos = mouseRay.GetPoint(distance);
+                    newPos = NearestUnitCoords(newPos);
+
+                    selectedBlock.transform.position = newPos;
+                }
+            }
+        }
+    }
+
+    private Vector3 NearestUnitCoords(Vector3 v)
+    {
+        v.x = Mathf.Round(v.x);
+        v.y = Mathf.Round(v.y);
+        v.z = Mathf.Round(v.z);
+        return v;
+    }
+
+    private void ChangePlayState()
+    {
+        playEnabled = !playEnabled;
+    }
+}
--- Assets/Scripts/Player/CameraLook.cs
+++ Assets/Scripts/Player/CameraLook.cs
@@ -1,50 +1,110 @@
 using System;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.InputSystem;
 
 public class CameraLook : MonoBehaviour
 {
+    [SerializeField] StateSelect stateSelect;
     [SerializeField] GameObject playerOrientation;
+    [SerializeField] GameObject flatCameraOrientation;
     [SerializeField] float xRotation = 33f;
-    [SerializeField] float mouseSensitivity = 15f;
+    [SerializeField] float mouseSensitivity = 5f;
     [SerializeField] float zoomSensitivity = 2f;
-    [SerializeField] float maxZoom = 22.5f;
+    [SerializeField] float maxZoom = 30f;
     [SerializeField] float minZoom = 1f;
+    [SerializeField] float moveSpeed = 0.5f;
 
     Camera cam;
 
     float _yRotation;
-    Boolean _rotateCam;
+    float radius = 50f;
+    float _zoom = 20f;
+    bool _rotateCam;
+    bool playEnabled = true;
+
+    Vector3 moveVector;
 
     private void Start()
     {
         cam = GetComponent<Camera>();
+        // transform.position = playerOrientation.transform.position - (transform.forward * radius) + Vector3.up;
+    }
+
+    private void OnEnable()
+    {
+        stateSelect.togglePressed += ChangePlayState;
+    }
+
+    private void OnDisable()
+    {
+        stateSelect.togglePressed -= ChangePlayState;
     }
 
     private void Update()
     {
         ZoomCamera();
         RotateCamera();
+
+        // Ensure no independent camera movement when player is in play mode
+        if (playEnabled)
+        {
+            transform.position = playerOrientation.transform.position - (transform.forward * radius) + Vector3.up;
+            return;
+        }
+
+        GetInput();
+    }
+
+  private void FixedUpdate()
+    {
+        // Ensure no independent camera movement when player is in play mode
+        if (playEnabled) return;
+
+        MoveCamera();
     }
 
     private void ZoomCamera()
     {
-        float zoomValue = cam.orthographicSize - Input.mouseScrollDelta.y * zoomSensitivity;
-        cam.orthographicSize = Mathf.Clamp(zoomValue, minZoom, maxZoom);
+        float newZoom = _zoom - Input.mouseScrollDelta.y * zoomSensitivity;
+        _zoom = Mathf.Clamp(newZoom, minZoom, maxZoom);
+        cam.orthographicSize = _zoom;
     }
 
     private void RotateCamera()
     {
         if (Input.GetMouseButtonDown(1)) _rotateCam = true;
         if (Input.GetMouseButtonUp(1)) _rotateCam = false;
-        
+
         if (_rotateCam)
         {
-            float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * mouseSensitivity;
-            _yRotation += mouseX;
-            transform.rotation = Quaternion.Euler(xRotation, _yRotation, 0); // Rotate cam
-            playerOrientation.transform.rotation = Quaternion.Euler(0, _yRotation, 0); // Rotate player's movement orientation
+            float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+            transform.RotateAround(playerOrientation.transform.position, Vector3.up, mouseX); // Rotate camera around player orienation
+
+            playerOrientation.transform.RotateAround(playerOrientation.transform.position, Vector3.up, mouseX); // Rotate player's movement orientation
         }
+    }
+
+    void GetInput()
+    {
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        moveVector.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MoveCamera()
+    {
+        // Set the move direction to be aligned with how the camera is rotated.
+        moveVector = playerOrientation.transform.right * moveVector.x + playerOrientation.transform.forward * moveVector.y;
+        moveVector.y = 0f;
+        moveVector = moveVector.normalized;
+
+        transform.position += moveVector;
+    }
+
+    private void ChangePlayState()
+    {
+        playEnabled = !playEnabled;
+        if (playEnabled) transform.position = playerOrientation.transform.position - (transform.forward * radius) + Vector3.up;
     }
 }
